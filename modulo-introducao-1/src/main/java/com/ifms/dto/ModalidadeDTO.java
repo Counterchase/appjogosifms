1d117a8a7a2ae6268bbacef66d03ad914a0878f6
counterchase <counterchase@COUNTERCHASE>
2021-04-07 23:46:50 -0300

    teste

---
+++ modulo-introducao-1/src/main/java/com/ifms/dto/ModalidadeDTO.java
@@ -0,0 +1,43 @@
+package com.ifms.dto;
+
+import java.io.Serializable;
+
+import com.ifms.entities.Modalidade;
+
+public class ModalidadeDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String descicao;
+	
+	private void ModalicadeDTO() {
+
+	}
+
+	public ModalidadeDTO(Long id, String descicao) {
+		this.id = id;
+		this.descicao = descicao;
+	}
+	public ModalidadeDTO(Modalidade entity) {
+		this.id = entity.getId();
+		this.descicao = entity.getDescricao();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescicao() {
+		return descicao;
+	}
+
+	public void setDescicao(String descicao) {
+		this.descicao = descicao;
+	}
+
+	
+}
